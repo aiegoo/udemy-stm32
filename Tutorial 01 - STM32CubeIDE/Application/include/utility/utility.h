b727eec32ea599030b6bf88df44bed79d49e3216
tocrux <38068303+tocrux@users.noreply.github.com>
2021-09-11 12:33:43 +0900

    Add basic form of stm32cubeide

---
+++ Tutorial 01 - STM32CubeIDE/Application/include/utility/utility.h
@@ -0,0 +1,19 @@
+/*
+ * utility.h
+ *
+ *  Created on: Sep 11, 2021
+ *      Author: tocru
+ */
+
+#ifndef INCLUDE_UTILITY_UTILITY_H_
+#define INCLUDE_UTILITY_UTILITY_H_
+
+#include "app_config.h"
+#include "stm32f4xx_hal.h"
+
+
+uint32_t millis(void);
+void delay_ms(uint32_t ms);
+
+
+#endif /* INCLUDE_UTILITY_UTILITY_H_ */
